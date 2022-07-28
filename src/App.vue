3444911bbfe99e0b2e090cdf9605303712441bfb
artem-brezutsky <a.brezutsky@gmail.com>
2022-07-28 18:56:02 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-    <div>Работает!</div>
+    <div>
+        <button v-on:click="addlike">Like</button>
+        <button v-on:click="addDisLike">DisLike</button>
+    </div>
+    <div>Кол-во лайков <strong>{{ likes }}</strong></div>
+    <div>Кол-во дизлайков <strong>{{ disLikes }}</strong></div>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            likes: 0,
+            disLikes: 5,
+        }
+    },
+    methods: {
+        addlike() {
+            this.likes++
+        },
+        addDisLike() {
+            this.disLikes++
+        },
+    },  
+}
 </script>
 
 <style>
